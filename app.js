da6544f8777a9ef811284e2b28db2ce9e5bc41bd
Roy-Tessler <23tessler@gmail.com>
2019-11-14 16:31:31 -0500

    part 4 - configuring routers

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const { db } = require("./models");
+const morgan = require("morgan");
+const { db, User, Page } = require("./models");
+const wikiRouter = require("./routes/wiki");
+const userRouter = require("./routes/user");
+app.use("/wiki", wikiRouter);
+app.use(morgan("dev"));
 
 app.use(express.static("public"));
 
-app.get("/", (rec, res, next) => {
-  res.send("sending from /");
+app.get("/", async (rec, res, next) => {
+  // const users = await User.findAll();
+  res.send("l");
 });
 
-app.listen(port, () => console.log(`listening on port ${port})`));
+const connect = async () => {
+  await User.sync({ force: true });
 
-db.authenticate().then(() => {
-  console.log("connected to the database");
-});
+  await Page.sync({ force: true });
+
+  app.listen(port, () => console.log(`listening on port ${port})`));
+
+  db.authenticate().then(() => {
+    console.log("connected to the database");
+  });
+};
+
+connect();
