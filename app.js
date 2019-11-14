e1d014e45f6cb2e467c095a9ef10cec8519f2e96
Roy-Tessler <23tessler@gmail.com>
2019-11-14 13:06:10 -0500

    added user and page

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const { db } = require("./models");
 
+app.use(express.static("public"));
 
-// app.use(express.static('public'))
+app.get("/", (rec, res, next) => {
+  res.send("sending from /");
+});
 
-app.get('/', (rec, res, next) => {
-  res.send("sending from /")
-})
+app.listen(port, () => console.log(`listening on port ${port})`));
 
-
-
-app.listen(port, () => console.log(`listening on port ${port})`)
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
