1c4084417fcb906add8a3865c3cd125040adf615
Emmanuel Kandilas <Ekandilas@gmail.com>
2019-11-14 12:26:55 -0500

    nodemon not loving it

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+
+// app.use(express.static('public'))
 
 app.get('/', (rec, res, next) => {
-  
+  res.send("sending from /")
 })
+
+
+
+app.listen(port, () => console.log(`listening on port ${port})`)
